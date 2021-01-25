1ed5faffe00ab42cf45f2088bea18c8404d6464e
kuznetov-g <backs92@mail.ru>
2021-01-25 23:14:28 +0500

    add .xib for collection view cell. And elements for cell

---
+++ Avito Test App/Controller/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  Avito Test App
+//
+//  Created by Георгий iMac on 25.01.2021.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
